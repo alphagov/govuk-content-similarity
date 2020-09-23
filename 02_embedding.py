e5d531e13034bdfc734fd84581a56f077bdf08ed
avisionh <a_vision@hotmail.co.uk>
2020-09-23 17:47:21 +0100

    Concatenate document vector array to base_path
    This is so we can include the base_path when visualising the SOMs.

--- 02_embedding.py
+++ 02_embedding.py
@@ -1,9 +1,9 @@
+from time import time
 from tqdm import tqdm
 import multiprocessing
 
 import pandas as pd
 import numpy as np
-from gensim.test.utils import get_tmpfile
 from gensim.models import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 
@@ -16,18 +16,27 @@ df = pd.read_csv(filepath_or_buffer='data/df.csv')
 # will use indices of sentences as tags
 documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['text_clean'])]
 
-# instantiate a doc2vec DBOW model
+# instantiate and train a doc2vec DBOW model
 model_dbow = Doc2Vec(dm=0, vector_size=300, negative=5, hs=0, min_count=2, sample=0, workers=n_cores)
 model_dbow.build_vocab([x for x in tqdm(documents)])
+t = time()
 model_dbow.train(documents, total_examples=model_dbow.corpus_count, epochs=1)
+print('Time to train: {} minutes'.format(round((time() - t) / 60, 2)))
 
 # save trained model
-file_name = get_tmpfile("my_doc2vec_model")
-model_dbow.save(file_name)
+model_dbow.save('data/model_dbow')
 #model_dbow = Doc2Vec.load(file_name)
 
-# store document vectors in numpy array for minisom
-# note: can't use list comprehension as it goes from 1-509,529
-array_doc_vectors = np.array([])
-for i in range(len(model_dbow.docvecs) - 1):
-    np.append(array_doc_vectors, model_dbow.docvecs[i])
+# store and save document vectors in numpy array for minisom
+array_doc_vectors = model_dbow.docvecs.vectors_docs
+np.save(file='data/document_vectors.npy', arr=array_doc_vectors)
+#array_doc_vectors = np.load(file='data/document_vectors.npy')
+
+# associate base_path with document vectors
+list_doc_vectors = array_doc_vectors.tolist()
+series_doc_vectors = pd.Series(list_doc_vectors)
+df_output = pd.DataFrame({'base_path': df['base_path'],
+                          'document_vectors': series_doc_vectors})
+
+# save dataframe
+pd.to_pickle(obj=df_output, filepath_or_buffer='data/df_document_vectors.pkl')
