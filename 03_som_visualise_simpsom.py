55593ab52af99cbfa23418f0cfb0e1f71359a2cf
avisionh <a_vision@hotmail.co.uk>
2020-09-25 22:27:02 +0100

    Add interactive plot
    This is so we can display the base paths for each cluster.

--- 03_som_visualise_simpsom.py
+++ 03_som_visualise_simpsom.py
@@ -1,25 +1,31 @@
 import SimpSOM as sps
 
-from math import sqrt
-
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import normalize
 
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-import seaborn as sns
-
 import plotly.offline as py
 import plotly.graph_objs as go
-import plotly.tools as tls
+
+from PIL import Image, ImageChops
+
+
+def auto_crop(filename):
+    im = Image.open(filename)
+    im = im.crop((0, 100, 2900, im.size[1]))
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+
 
 df_output = pd.read_pickle(filepath_or_buffer='data/df_document_vectors.pkl')
 array_doc_vec = np.load(file='data/document_vectors.npy')
 
-labels = df_output['base_path'][:500]
+labels = df_output['base_path'][:500].tolist()
 
 # normalise array using l2 normalisation
 # so sum of squares is 1 for each vector
@@ -44,4 +50,46 @@ net.diff_graph(show=True, printout=True)
 net.save(fileName='somNet_trained', path='././data')
 
 # project our data onto the map to see where pages are being mapped
-plot_data = net.project(normed_array_doc_vec)
+plot_data = net.project(array=normed_array_doc_vec)
+
+# interactive plotting to see base_paths
+img_crop = auto_crop('nodesDifference.png')
+img_crop.save('reports/figures/som_nodes_difference_crop.png')
+
+# prepare plotly graph
+trace = go.Scatter(
+    x=[x for x, y in plot_data],
+    y=[y for x, y in plot_data],
+    hovertext=[str(n) for n in labels],
+    text=[str(n) for n in labels],
+    mode='markers',
+    marker=dict(
+        size=8,
+        color='white',
+        opacity=1
+    ),
+    showlegend=False
+)
+
+data = [trace]
+
+layout = go.Layout(
+    images=[dict(source='reports/figures/som_nodes_difference_crop.png',
+                 xref='x',
+                 yref='y',
+                 x=-0.5,
+                 y=39.5*2/np.sqrt(3)*3/4,
+                 sizex=40.5,
+                 sizey=40*2/np.sqrt(3)*3/4,
+                 sizing='stretch',
+                 opacity=0.5,
+                 layer='below')],
+    width=800,
+    height=800,
+    hovermode='closest',
+    xaxis=dict(range=[-1, 41], zeroline=False, showgrid=False, ticks='', showticklabels=False),
+    yaxis=dict(range=[-1, 41], zeroline=False, showgrid=False, ticks='', showticklabels=False),
+    showlegend=True
+)
+fig = dict(data=data, layout=layout)
+py.plot(figure_or_data=fig, filename='styled-scatter.html')
