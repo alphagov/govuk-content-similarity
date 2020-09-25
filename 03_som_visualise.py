13a3dd425f3e0c94313c8913fbefd62e33ab96e0
avisionh <a_vision@hotmail.co.uk>
2020-09-25 11:04:01 +0100

    Create function to compute minimum distance between factors
    This is so we can input a more 'square-like' gridsize into our MiniSom to get a more better plot.

--- 03_som_visualise.py
+++ 03_som_visualise.py
@@ -5,6 +5,31 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import cm, colorbar
+
+
+def get_minimal_distance_factors(n):
+    """
+    Get the factors of a number which have the smallest difference between them.
+    This is so we specify the gridsize for our SOM to be relatively balanced in height and width.
+
+    :param n: Integer or float we want to extract the closest factors from.
+    :return: The factors of n whose distance from each other is minimised.
+    """
+    try:
+        if isinstance(n, float):
+            n = int(round(n, 0))
+            return get_minimal_distance_factors(n)
+        else:
+            root = np.floor(sqrt(n))
+            while n % root > 0:
+                root -= 1
+            return root, n/root
+    except TypeError:
+        print("Input '{}' is not a integer nor float, please enter one!".format(n))
+
 
 df_output = pd.read_pickle(filepath_or_buffer='data/df_document_vectors.pkl')
 array_doc_vectors = np.load(file='data/document_vectors.npy')
@@ -28,10 +53,17 @@ y = total_neurons/x
 x = int(round(x, 0))
 y = int(round(y, 0))
 
+
+# values of x and y are too far from each other
+x, y = get_minimal_distance_factors(total_neurons)
+
+
 del total_neurons, normal_cov, eigen_values, result
 
 # initialization and training of SOM
-som = MiniSom(x=x, y=y, input_len=len_vector, sigma=0.3, learning_rate=0.5, random_seed=42)
+som = MiniSom(x=x, y=y, input_len=len_vector,
+              activation_distance='cosine', topology='hexagonal',
+              sigma=0.3, learning_rate=0.5, random_seed=42)
 # initialise weights to map
 som.pca_weights_init(data=array_doc_vectors)
 som.train_batch(data=array_doc_vectors, num_iteration=100)
@@ -39,4 +71,43 @@ som.train_batch(data=array_doc_vectors, num_iteration=100)
 # basic plot
 plt.figure(figsize=(10, 10))
 plt.pcolor(som.distance_map().T, cmap='bone_r')
+plt.show()
+
+# hexagonal plotting
+f = plt.figure(figsize=(10, 10))
+ax = f.add_subplot(111)
+ax.set_aspect('equal')
+
+xx, yy = som.get_euclidean_coordinates()
+umatrix = som.distance_map()
+weights = som.get_weights()
+
+for i in range(weights.shape[0]):
+    for j in range(weights.shape[1]):
+        wy = yy[(i, j)]*2/np.sqrt(3)*3/4
+        hex = RegularPolygon((xx[(i, j)], wy), numVertices=6, radius=.95/np.sqrt(3),
+                             facecolor=cm.Blues(umatrix[i, j]), alpha=.4, edgecolor='gray')
+        ax.add_patch(hex)
+
+for cnt, x in enumerate(array_doc_vectors):
+    w = som.winner(x)  # getting the winner
+    # place a marker on the winning position for the sample xx
+    wx, wy = som.convert_map_to_euclidean(w)
+    wy = wy*2/np.sqrt(3)*3/4
+    plt.plot(wx, wy, markerfacecolor='None', markersize=12, markeredgewidth=2)
+
+xrange = np.arange(weights.shape[0])
+yrange = np.arange(weights.shape[1])
+plt.xticks(xrange-.5, xrange)
+plt.yticks(yrange*2/np.sqrt(3)*3/4, yrange)
+
+divider = make_axes_locatable(plt.gca())
+ax_cb = divider.new_horizontal(size="5%", pad=0.05)
+cb1 = colorbar.ColorbarBase(ax_cb, cmap=cm.Blues,
+                            orientation='vertical', alpha=.4)
+cb1.ax.get_yaxis().labelpad = 16
+cb1.ax.set_ylabel('distance from neurons in the neighbourhood',
+                  rotation=270, fontsize=16)
+plt.gcf().add_axes(ax_cb)
+
 plt.show()
