8c5edc7bf96d82b662e137b0c4e127278ef47bb8
avisionh <a_vision@hotmail.co.uk>
2020-09-28 17:28:58 +0100

    Overlay plotly on SOM diff graph
    This is so we can visualise the clusters.

--- 03_som_visualise_simpsom.py
+++ 03_som_visualise_simpsom.py
@@ -1,5 +1,6 @@
 import SimpSOM as sps
 
+import pickle
 import numpy as np
 import pandas as pd
 
@@ -11,12 +12,22 @@ import plotly.graph_objs as go
 from PIL import Image, ImageChops
 
 
-def auto_crop(filename):
+def auto_crop(filename, left, right, top):
+    """
+    Crop image passed in and overlay a blank canvas
+
+    param filename: Name of image file to crop.
+    param left: The amount of image you want to keep from left-side
+    param right: The amount of image you want to keep from the right-side
+    param top: The amount of image you want to keep from the top
+    return: Cropped empty canvas for interactive plotting
+
+    """
     im = Image.open(filename)
-    im = im.crop((0, 100, 2900, im.size[1]))
-    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
+    im = im.crop(box=(left, right, top, im.size[1]))
+    bg = Image.new(mode=im.mode, size=im.size, color=im.getpixel((0, 0)))
+    diff = ImageChops.difference(image1=im, image2=bg)
+    diff = ImageChops.add(image1=diff, image2=diff, scale=2.0, offset=-100)
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
@@ -46,50 +57,53 @@ net.train(startLearnRate=0.1, epochs=5000)
 # which will help identify cluster centres
 net.diff_graph(show=True, printout=True)
 
-# save the network dimensions, PBC and node weights to file
-net.save(fileName='somNet_trained', path='././data')
+# save the som
+with open('././data/som_net_train.pkl', 'wb') as outfile:
+    pickle.dump(net, outfile)
+
+# load trained model
+with open('././data/som_net_train.pkl', 'rb') as infile:
+    test = pickle.load(infile)
 
 # project our data onto the map to see where pages are being mapped
 plot_data = net.project(array=normed_array_doc_vec)
 
 # interactive plotting to see base_paths
-img_crop = auto_crop('nodesDifference.png')
+img_crop = auto_crop(filename='nodesDifference.png',
+                     left=0, right=0, top=2900)
 img_crop.save('reports/figures/som_nodes_difference_crop.png')
 
 # prepare plotly graph
-trace = go.Scatter(
-    x=[x for x, y in plot_data],
-    y=[y for x, y in plot_data],
-    hovertext=[str(n) for n in labels],
-    text=[str(n) for n in labels],
-    mode='markers',
-    marker=dict(
-        size=8,
-        color='white',
-        opacity=1
-    ),
-    showlegend=False
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(
+        x=[x for x, y in plot_data],
+        y=[y for x, y in plot_data],
+        hovertext=[str(n) for n in labels],
+        text=[str(n) for n in labels],
+        mode='markers',
+        marker=dict(
+            size=8,
+            color='white',
+            opacity=1),
+        showlegend=False)
 )
 
-data = [trace]
-
-layout = go.Layout(
-    images=[dict(source='reports/figures/som_nodes_difference_crop.png',
-                 xref='x',
-                 yref='y',
-                 x=-0.5,
-                 y=39.5*2/np.sqrt(3)*3/4,
-                 sizex=40.5,
-                 sizey=40*2/np.sqrt(3)*3/4,
-                 sizing='stretch',
-                 opacity=0.5,
-                 layer='below')],
-    width=800,
-    height=800,
-    hovermode='closest',
-    xaxis=dict(range=[-1, 41], zeroline=False, showgrid=False, ticks='', showticklabels=False),
-    yaxis=dict(range=[-1, 41], zeroline=False, showgrid=False, ticks='', showticklabels=False),
-    showlegend=True
+# add images
+im = Image.open(fp='nodesDifference.png')
+fig.add_layout_image(
+        dict(
+            source=im,
+            xref="x",
+            yref="y",
+            x=0,
+            y=3,
+            sizex=2,
+            sizey=2,
+            sizing="stretch",
+            opacity=0.5,
+            layer="below")
 )
-fig = dict(data=data, layout=layout)
+
 py.plot(figure_or_data=fig, filename='styled-scatter.html')
