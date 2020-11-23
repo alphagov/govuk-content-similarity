241b1665da2da6cf0176501b5749393f312a2b84
avisionh <a_vision@hotmail.co.uk>
2020-11-23 09:28:01 +0000

    Move constants to separate script
    This is to make code neater. Also update packages and rename folders plus files so project is better organised.

--- notebooks/soms/01_load_clean.py
+++ notebooks/soms/01_load_clean.py
@@ -1,3 +1,5 @@
+from src.utils.constants import CONTENT_STORE_HEADER, CONTENT_STORE_DATE
+
 from time import time
 import os
 import re
@@ -10,13 +12,15 @@ import spacy
 def clean_text(doc):
     """
     Lemmatises and remove stopwords.
+    Note: Have not moved to a separate functions script as this function depends on creation of spacy Doc object \n
+          haven't quite figured out a way to isolate that appropriately if moved to a separate script.
 
     :param doc: spacy Doc object to lemmatise and remove stop words from
     :return: spacy Doc object that has been lemmatised and have had stop words removed
     """
 
     txt = [token.lemma_ for token in doc if not token.is_stop]
-    # remove one or two word sentences as are not beneficial to @ord2Vec
+    # remove one or two word sentences as are not beneficial to Word2Vec
     if len(txt) > 2:
         return ' '.join(txt)
 
@@ -25,44 +29,15 @@ DATA_DIR = os.getenv("DATA_DIR")
 FILE_NAME = "preprocessed_content_store_180920.csv.gz"
 n_cores = multiprocessing.cpu_count() - 1
 
-dict_header = {'base_path': object,
-               'content_id': object,
-               'title': object,
-               'description': object,
-               'publishing_app': object,
-               'document_type': object,
-               'details': object,
-               'text': object,
-               'organisations': object,
-               'taxons': object,
-               'step_by_steps': object,
-               'details_parts': object,
-               'first_published_at': object,
-               'public_updated_at': object,
-               'updated_at': object,
-               'finder': object,
-               'facet_values': object,
-               'facet_groups': object,
-               'has_brexit_no_deal_notice': bool,
-               'withdrawn': bool,
-               'withdrawn_at': object,
-               'withdrawn_explanation': object}
-list_header_date = ['first_published_at',
-                    'public_updated_at',
-                    'updated_at',
-                    'withdrawn_at']
-
 # load data
 df = pd.read_csv(filepath_or_buffer=DATA_DIR + "/" + FILE_NAME,
                  compression='gzip',
                  encoding='utf-8',
                  sep='\t',
                  header=0,
-                 names=list(dict_header.keys()),
-                 dtype=dict_header,
-                 parse_dates=list_header_date)
-
-del DATA_DIR, FILE_NAME, dict_header, list_header_date
+                 names=list(CONTENT_STORE_HEADER.keys()),
+                 dtype=CONTENT_STORE_HEADER,
+                 parse_dates=CONTENT_STORE_DATE)
 
 # remove rows with no text
 df = df.dropna(subset=['text'], axis=0)
@@ -75,8 +50,7 @@ nlp = spacy.load('en', disable=['ner', 'parser'])
 # lemmatise and remove stopwords
 t = time()
 df['text_clean'] = [clean_text(doc) for doc in nlp.pipe(df['text_clean'], batch_size=5000)]
-print('Time to clean up everything: {} minutes'.format(round((time() - t) / 60, 2)))
-del t
+print(f'Time to clean up everything: {round((time() - t) / 60, 2)} minutes')
 
 # remove NAs and duplicates
 df = df.dropna(axis=0, subset=['text_clean']).drop_duplicates()
